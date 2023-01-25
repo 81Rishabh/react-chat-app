fdaa7886f56e7eadcb6c9c35a72ddf0d26e073a7
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2023-01-25 14:07:54 +0530

    add server_url

--- src/socket.js
+++ src/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
-const URL = "http://localhost:5000";
+const URL = "https://chit-chat-server-7pqo.onrender.com";
 
 const socket = io(URL , {autoConnect : false});
 
