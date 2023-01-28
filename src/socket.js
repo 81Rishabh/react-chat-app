f7ff3373105ce909dd71c7e8d0aa86b7a806ad73
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2023-01-28 14:44:58 +0530

    intial commit

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,11 @@
 import { io } from "socket.io-client";
-// const URL = "https://chit-chat-server-7pqo.onrender.com";
-const URL = "http://localhost:8080";
-const socket = io(URL , {autoConnect : false});
+
+const socket = io(
+    'http://localhost:8080',
+     {
+        autoConnect : false,
+    }
+ );
 
 socket.onAny((event, ...args) => {
     console.log(event, args);
