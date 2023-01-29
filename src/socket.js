247e7d41fb755c7200945ec7035e9e551bf4fad4
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2023-01-29 12:38:50 +0530

    url changed

--- src/socket.js
+++ src/socket.js
@@ -1,14 +1,8 @@
 import { io } from "socket.io-client";
-// const url = 'https://chit-chat-server-7pqo.onrender.com';
-const local_url = 'http://localhost:8080';
+const URL = 'https://chit-chat-server-7pqo.onrender.com';
+// const URL = 'http://localhost:8080';
 
-const socket = io(local_url , {autoConnect : false});
-
-socket.on("connect_error", (err) => {
-    console.log(err);
-    // revert to classic upgrade
-    socket.io.opts.transports = ["polling", "websocket"];
-});
+const socket = io(URL , {autoConnect : false});
 
 socket.onAny((event, ...args) => {
     console.log(event, args);
