318b374a954cd7f7e78499e716c3321531037386
rishabh vishwakarma <vishwakarmarishabh378@gmail.com>
2023-01-29 12:58:25 +0530

    add transport to websocket

--- src/socket.js
+++ src/socket.js
@@ -2,7 +2,11 @@ import { io } from "socket.io-client";
 // const URL = 'https://chit-chat-server-7pqo.onrender.com';
 const URL = 'http://localhost:8080';
 
-const socket = io(URL , {autoConnect : false});
+const socket = io(URL , {
+    autoConnect : false,
+    transports : ['websocket'],
+    upgrade : false
+});
 
 socket.onAny((event, ...args) => {
     console.log(event, args);
